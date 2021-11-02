96d7909854e142a6a87dfcc680220812666f463a
Scala Steward <me@scala-steward.org>
2021-11-02 04:37:40 +0100

    Update scala-library to 2.13.7

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,7 +9,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
   val Scala212   = "2.12.15"
-  val Scala213   = "2.13.5"
+  val Scala213   = "2.13.7"
   val ScalaDotty = "3.0.1"
 
   private val stdOptions = Seq(
