ce3eed4d47a8aa4e185f9a92be284e8a1ecbe9e8
Scala Steward <me@scala-steward.org>
2022-04-12 21:36:47 +0200

    Update scala3-library to 3.1.2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -10,7 +10,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
   val Scala212   = "2.12.15"
   val Scala213   = "2.13.5"
-  val ScalaDotty = "3.1.0"
+  val ScalaDotty = "3.1.2"
 
   private val stdOptions = Seq(
     "-deprecation",
