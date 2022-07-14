95b294e69811775eade99c120107a16f624095f6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-14 14:28:38 +0200

    Update scala-library to 2.13.8 (#333)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,7 +9,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
   val Scala212   = "2.12.15"
-  val Scala213   = "2.13.5"
+  val Scala213   = "2.13.8"
   val ScalaDotty = "3.1.2"
 
   private val stdOptions = Seq(
