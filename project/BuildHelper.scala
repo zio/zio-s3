a45638920a1d0df4561b92e4a9b96f35af21995c
Scala Steward <scala_steward@virtuslab.com>
2022-07-14 07:52:07 +0000

    Update scala-library to 2.12.16

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ import BuildInfoKeys._
 import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
-  val Scala212   = "2.12.15"
+  val Scala212   = "2.12.16"
   val Scala213   = "2.13.5"
   val ScalaDotty = "3.1.2"
 
