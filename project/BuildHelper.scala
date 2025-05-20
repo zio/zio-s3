8ceca76102b160b7dfac71a6f15f44e7822962a6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-20 06:38:23 +0200

    Update scala-library to 2.12.20 (#509)
    Co-authored-by: Adam Hearn <22334119+hearnadam@users.noreply.github.com>

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ import BuildInfoKeys._
 import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
-  val Scala212   = "2.12.15"
+  val Scala212   = "2.12.20"
   val Scala213   = "2.13.14"
   val ScalaDotty = "3.3.3"
 
