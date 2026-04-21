eb9eee9f85cf1a8a952a172294b74e178d1ef38c
Scala Steward <scala_steward@virtuslab.com>
2026-04-21 21:08:21 +0000

    Update scala-library to 2.13.18

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,7 +9,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
   val Scala212   = "2.12.20"
-  val Scala213   = "2.13.16"
+  val Scala213   = "2.13.18"
   val ScalaDotty = "3.3.6"
 
   private val stdOptions = Seq(
